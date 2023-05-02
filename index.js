2af5514eb3f30560a3921a075f3c851965fbc7e7
reza107-hub <rashidaminur104@gmail.com>
2023-05-02 17:33:10 +0600

    chef json done

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
+const chef = require('./chef.json')
 app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+app.get('/chef', (req, res) => {
+    res.send(chef)
 })
 
 app.listen(port, () => {
